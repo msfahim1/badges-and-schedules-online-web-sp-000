0afdc8e634da9fc9c1d7449ae66c8bf255352fd5
Michael Fahim <michael_fahim@yahoo.com>
2019-11-27 17:41:43 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,12 @@
 # Write your code here.
+
+def badge_maker(name)
+
+ return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(name_array)
+  name_array.each do |name|
+    puts badge_maker(name)
+  end
+end
